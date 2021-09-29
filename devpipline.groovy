e50827895b5ed9745f7d929124bc065da25bd970
bjcgd2020 <bjcgd@126.com>
2021-09-29 10:53:12 +0800

    增加devlib测试

---
+++ devpipline.groovy
@@ -0,0 +1,107 @@
+#!groovy
+@Library('devopslib') _
+
+def tools = new org.devops.tools() 
+
+String workspace = "/opt/jenkins/workspace"
+//Pipeline
+pipeline {
+    agent { node {  label "master"   //指定运行节点的标签或者名称
+                    customWorkspace "${workspace}"   //指定运行工作目录（可选）
+            }
+    }
+    
+    parameters { string(name: 'DEPLOY_ENV', defaultValue: 'staging', description: '') }
+
+    options {
+        timestamps()  //日志会有时间
+        skipDefaultCheckout()  //删除隐式checkout scm语句
+        disableConcurrentBuilds() //禁止并行
+        timeout(time: 1, unit: 'HOURS')  //流水线超时设置1h
+    }
+    
+   
+    stages {
+        //下载代码
+        stage("GetCode"){ //阶段名称
+          
+            when { environment name: 'test', value: 'abcd' }
+             
+            steps{  //步骤
+                timeout(time:5, unit:"MINUTES"){   //步骤超时时间
+                    script{ //填写运行代码
+                        println('获取代码')
+                        println("${test}")
+                        println(" ${params.DEPLOY_ENV}")
+                        mvnhome = tool "mvn382"
+                        println(mvnhome)
+                        input id: 'Envid', message: '请选择编译环境', ok: '继续编译', parameters: [choice(choices: ['java', 'pyhton', 'php'], name: 'envname')], submitter: 'admin'
+                        //echo "Hello, ${envname}, nice to meet you."
+                        tools.PrintMes('发布应用!','green')
+					    
+                
+                    }
+                }
+            }
+        }
+
+        stage("01"){
+            failFast true
+            parallel {
+        
+                //构建
+                stage("Build"){
+                    steps{
+                        timeout(time:20, unit:"MINUTES"){
+                            script{
+                                println('应用打包')
+                                
+             
+                            }
+                        }
+                    }
+                }
+        
+                //代码扫描
+                stage("CodeScan"){
+                    steps{
+                        timeout(time:30, unit:"MINUTES"){
+                            script{
+                                print("代码扫描")
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //构建后操作
+    post {
+        always {
+            script{
+                println("always")
+            }
+        }
+
+        success {
+            script{
+                currentBuild.description = "\n 构建成功!" 
+            }
+        }
+
+        failure {
+            script{
+                currentBuild.description = "\n 构建失败!" 
+            }
+        }
+
+        aborted {
+            script{
+                currentBuild.description = "\n 构建取消!" 
+            }
+        }
+    }
+}
+
